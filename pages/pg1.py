eb69b14d4a3a64446da7236b38ec050dcf735761
syduc993 <106389091+syduc993@users.noreply.github.com>
2023-06-28 11:26:02 +0700

    ok

--- pages/pg1.py
+++ pages/pg1.py
@@ -162,12 +162,12 @@ def update_figure(sub_group_selected,product_selected,rsm_selected,am_selected,s
 
     data = data.groupby('Date').agg(pl.col("Số lượng bán","Số lượng nhập","Số lượng thực hủy","Tồn kho siêu thị").sum()).to_pandas().sort_values(by="Date",ascending=True).reset_index().drop(columns=['index'])
 
-    fig.add_trace(go.Scatter(x = data["Date"], y = data["Số lượng nhập"], fill='tozeroy',showlegend=False),row=1, col=1)
-    fig.add_trace(go.Scatter(x = data["Date"], y = data["Số lượng bán"], fill='tozeroy' ,showlegend=False),row=1, col=2)
-    fig.add_trace(go.Scatter(x = data["Date"], y = data["Tồn kho siêu thị"], fill='tozeroy',showlegend=False),row=2, col=1)
-    fig.add_trace(go.Scatter(x = data["Date"], y = data["Số lượng thực hủy"], fill='tozeroy' ,showlegend=False),row=2, col=2)
+    fig.add_trace(go.Scatter(x = df["Date"], y = df["Số lượng nhập"], fill='tozeroy',showlegend=False),row=1, col=1)
+    fig.add_trace(go.Scatter(x = df["Date"], y = df["Số lượng bán"], fill='tozeroy' ,showlegend=False),row=1, col=2)
+    fig.add_trace(go.Scatter(x = df["Date"], y = df["Tồn kho siêu thị"], fill='tozeroy',showlegend=False),row=2, col=1)
+    fig.add_trace(go.Scatter(x = df["Date"], y = df["Số lượng thực hủy"], fill='tozeroy' ,showlegend=False),row=2, col=2)
 
-    # fig.update_layout(title=rsm_selected)
-    # fig.update_layout(width=1550, height=800)
+    fig.update_layout(title=rsm_selected)
+    fig.update_layout(width=1550, height=800)
  
     return fig
